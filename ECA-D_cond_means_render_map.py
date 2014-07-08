e164a3b42b3fdbbce3f66abd08de30d73d21416d
jajcayn <jajcay@cs.cas.cz>
2014-07-08 16:18:26 +0200

    render map

--- ECA-D_cond_means_render_map.py
+++ ECA-D_cond_means_render_map.py
@@ -6,6 +6,31 @@ created on June 13, 2014
 
 import cPickle
 from datetime import datetime, date
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+
+
+def render_differences_map(diffs, surr_diffs, lats, lons, fname = None):
+    fig = plt.figure(figsize=(15,12))
+    lat_ndx = np.argsort(lats)
+    lats = lats[lat_ndx]
+    m = Basemap(projection = 'merc',
+                llcrnrlat = lats[0], urcrnrlat = lats[-1],
+                llcrnrlon = lons[0], urcrnrlon = lons[-1],
+                resolution = 'c')
+                
+    x, y = m(*np.meshgrid(lons, lats))
+    cs = m.contourf(x, y, diffs, levels = np.arange(-1,1,0.1))
+    plt.colorbar(cs)
+    m.drawcoastlines(linewidth = 1.5)
+    m.drawmapboundary()
+    plt.title('test map')
+    
+    if fname != None:
+        plt.savefig(fname)
+    else:
+        plt.show()
 
 
 
@@ -24,6 +49,8 @@ with open(fname, 'rb') as f:
     data = cPickle.load(f)
 difference_data = data['difference_data']
 mean_data = data['mean_data']
+lats = data['lats']
+lons = data['lons']
 del data
 
 # load surrogates
@@ -36,4 +63,6 @@ difference_surrogates = data['difference_surrogates']
 mean_surrogates = data['mean surrogates']
 del data
 print("[%s] Surrogates loaded." % (str(datetime.now())))
+
+render_differences_map(mean_data, None, lats, lons, 'test.png')
 
--- scaling_in_bins.py
+++ scaling_in_bins.py
@@ -61,9 +61,11 @@ def render_extremes_and_scaling_in_bins(res, scaling, fname = None):
         ax.tick_params(top = 'off', right = 'off', color = '#6A4A3C')
         ax.tick_params(which = 'minor', top = 'off', right = 'off', color = '#6A4A3C')
         ax.loglog(scaling[i, :], linewidth = 2, color = '#6A4A3C')
-        ax.axis([0, scaling.shape[1], 0, 6])
+#        ax.axis([0, scaling.shape[1], 0, 6])
         ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
         ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
+        ax.set_ylim(ymax = 6)
+        ax.set_xlim(xmax = 80)
         ax.set_xlabel('log $\Delta$time [days]')
         ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
         if i > 0 and i < scaling.shape[0]-1:
@@ -83,13 +85,89 @@ def render_extremes_and_scaling_in_bins(res, scaling, fname = None):
         
         
 def render_scaling_min_max(scaling, min_scaling, max_scaling, fname = None):
-    fig = plt.figure(figsize = (13,5), frameon = False)
+    fig = plt.figure(figsize = (14,9), frameon = False)
+    
+    colours = ["#E3D2B4", "#AC9F7F", "#ACECC9", 
+               "#FFBF17", "#FF4F01", "#F32645", 
+               "#C42366", "#A92477"]
+    
+    gs = gridspec.GridSpec(1, 3)
+    gs.update(left = 0.05, right = 0.95, top = 0.85, bottom = 0.3, wspace = 0.2)
+    ax1 = plt.Subplot(fig, gs[0,1])
+    fig.add_subplot(ax1)
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    ax1.spines['left'].set_visible(False)
+    ax1.tick_params(top = 'off', right = 'off', color = '#6A4A3C')
+    ax1.tick_params(which = 'minor', top = 'off', right = 'off', color = '#6A4A3C')
+    lab = {}
+    for i in range(scaling.shape[0]):
+        lab[i], = ax1.loglog(scaling[i, 0:], linewidth = 0.75, color = colours[i])
+#    ax1.axis([0, scaling.shape[1], 0, 6])
+    ax1.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax1.yaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax1.set_ylim(ymax = 6)
+    ax1.set_xlim(xmax = 80)
+    ax1.set_xlabel('log $\Delta$time [days]')
+    ax1.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+    ax1.set_title("scaling mean temperature TG")
+    fig.legend([lab[i] for i in range(scaling.shape[0])], 
+                ["bin %d: %.2f - %.2f" % (i+1, phase_bins[i], phase_bins[i+1]) for i in range(scaling.shape[0])], 
+                 loc = 'lower center')
+    
+    ax2 = plt.Subplot(fig, gs[0,0])
+    fig.add_subplot(ax2)
+    ax2.spines['top'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
+    ax2.tick_params(top = 'off', right = 'off', color = '#6A4A3C')
+    ax2.tick_params(which = 'minor', top = 'off', right = 'off', color = '#6A4A3C')
+    for i in range(scaling.shape[0]):
+        ax2.loglog(min_scaling[i, 0:], linewidth = 0.75, color = colours[i])
+#    ax2.axis([0, scaling.shape[1], 0, 6])
+    ax2.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax2.yaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax2.set_ylim(ymax = 6)
+    ax2.set_xlim(xmax = 80)
+    ax2.set_xlabel('log $\Delta$time [days]')
+    ax2.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+    ax2.set_title("scaling min temperature TN")
+    
+    ax3 = plt.Subplot(fig, gs[0,2])
+    fig.add_subplot(ax3)
+    ax3.spines['top'].set_visible(False)
+    ax3.spines['right'].set_visible(False)
+    ax3.spines['left'].set_visible(False)
+    ax3.tick_params(top = 'off', right = 'off', color = '#6A4A3C')
+    ax3.tick_params(which = 'minor', top = 'off', right = 'off', color = '#6A4A3C')
+    for i in range(scaling.shape[0]):
+        ax3.loglog(max_scaling[i, 0:], linewidth = 0.75, color = colours[i])
+#    ax3.axis([0, scaling.shape[1], 0, 6])
+    ax3.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax3.yaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax3.set_ylim(ymax = 6)
+    ax3.set_xlim(xmax = 80)
+    ax3.set_xlabel('log $\Delta$time [days]')
+    ax3.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+    ax3.set_title("scaling max temperature TX")
+    
+    fig.text(0.02, 0.575, 'log $\Delta$difference [$^{\circ}$C]', ha = 'center', va = 'center', rotation = 'vertical')
+    fig.text(0.97, 0.575, 'log $\Delta$difference [$^{\circ}$C]', ha = 'center', va = 'center', rotation = -90)
+    
+    plt.suptitle('%s - %d point / %s window: %s -- %s' % (g.location, MIDDLE_YEAR, '14k' if WINDOW_LENGTH < 16000 else '16k', 
+                  str(g.get_date_from_ndx(0)), str(g.get_date_from_ndx(-1))), size = 16)
+                  
+    if fname != None:
+        plt.savefig(fname)
+    else:
+        plt.show()
+    
 
 
 PERIOD = 8
-WINDOW_LENGTH = 16384 # 13462, 16384
-MIDDLE_YEAR = 1965 # year around which the window will be deployed
-JUST_SCALING = False
+WINDOW_LENGTH = 13462 # 13462, 16384
+MIDDLE_YEAR = 1969 # year around which the window will be deployed
+JUST_SCALING = True
 
 
 # load whole data
@@ -139,12 +217,15 @@ result = np.zeros((8, 6)) # bin no. x result no.
 # binning
 phase_bins = get_equidistant_bins()
 scaling = []
+if JUST_SCALING:
+    scaling_min = []
+    scaling_max = []
 for i in range(phase_bins.shape[0] - 1):
     ndx = ((phase >= phase_bins[i]) & (phase <= phase_bins[i+1]))
     data_temp = g.data[ndx].copy()
     time_temp = g.time[ndx].copy()
     max_temp = g_max.data[ndx]
-    min_temp = g_max.data[ndx]
+    min_temp = g_min.data[ndx]
     
     if not JUST_SCALING:
         # positive extremes
@@ -162,7 +243,7 @@ for i in range(phase_bins.shape[0] - 1):
                 result[i, 5] += 1
             
     #scaling
-    scaling_bin = []
+    scaling_bin = [0]
     for diff in range(1,80):
         difs = []
         for day in range(data_temp.shape[0]-diff):
@@ -171,12 +252,41 @@ for i in range(phase_bins.shape[0] - 1):
         difs = np.array(difs)
         scaling_bin.append(np.mean(difs))
     scaling.append(scaling_bin)
+    
+    if JUST_SCALING:
+        scaling_bin = [0]
+        for diff in range(1,80):
+            difs = []
+            for day in range(min_temp.shape[0]-diff):
+                if (time_temp[day+diff] - time_temp[day]) == diff:
+                    difs.append(np.abs(min_temp[day+diff] - min_temp[day]))
+            difs = np.array(difs)
+            scaling_bin.append(np.mean(difs))
+        scaling_min.append(scaling_bin)
+        
+        scaling_bin = [0]
+        for diff in range(1,80):
+            difs = []
+            for day in range(max_temp.shape[0]-diff):
+                if (time_temp[day+diff] - time_temp[day]) == diff:
+                    difs.append(np.abs(max_temp[day+diff] - max_temp[day]))
+            difs = np.array(difs)
+            scaling_bin.append(np.mean(difs))
+        scaling_max.append(scaling_bin)
+        
 
 scaling = np.array(scaling)
+if JUST_SCALING:
+    scaling_min = np.array(scaling_min)
+    scaling_max = np.array(scaling_max)
+
 
 if not JUST_SCALING:
     fname = ('debug/scaling_extremes_%d_%sk_window.png' % (MIDDLE_YEAR, '14' if WINDOW_LENGTH < 16000 else '16'))
     render_extremes_and_scaling_in_bins(result, scaling, fname)
+else:
+    fname = ('debug/scaling_min_max_%d_%sk_window.png' % (MIDDLE_YEAR, '14' if WINDOW_LENGTH < 16000 else '16'))
+    render_scaling_min_max(scaling, scaling_min, scaling_max, fname)
             
 
 
