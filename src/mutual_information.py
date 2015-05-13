6a5ae63c76f64d8468ef61e425a338b1f3604a1c
jajcayn <jajcay@cs.cas.cz>
2015-05-13 14:14:34 +0200

    added other eqq algorithm

--- src/mutual_information.py
+++ src/mutual_information.py
@@ -69,7 +69,13 @@ def cond_mutual_information(x, y, z, algorithm = 'EQQ', bins = 8, log2 = True):
         I(x; y | z) = sum( p(x,y,z) * log( p(z)*p(x,y,z) / p(x,z)*p(y,z) ),
         where p(z), p(x,z), p(y,z) and p(x,y,z) are probability distributions.
     The probability distributions could be estimated using these algorithms:
-        equiquantal binning - algorithm keyword 'EQQ'
+        equiquantal binning - algorithm keyword 'EQQ' or 'EQQ2'
+            EQQ - equiquantality is forced (even if many samples have the same value 
+                at and near the bin edge), can happen that samples with same value fall
+                into different bin
+            EQQ2 - if more than one sample has the same value at the bin edge, the edge is shifted,
+                so that all samples with the same value fall into the same bin, can happen that bins
+                do not necessarily contain the same amount of samples
         equidistant binning - algorithm keyword 'EQD'
         (preparing more...)
     If log2 is True (default), the units of cond. mutual information are bits, if False
@@ -114,6 +120,84 @@ def cond_mutual_information(x, y, z, algorithm = 'EQQ', bins = 8, log2 = True):
 
         yz_bins = [y_bin]
         [yz_bins.append(cond_bins) for cond_bins in z_bins]
+
+
+    elif algorithm == 'EQQ2':
+        # create EQQ bins -- condition [possibly multidimensional]
+        z_bins = [] # arrays of bins for all conditions
+        for cond in range(z.shape[0]):
+            z_sorted = np.sort(z[cond, :])
+            z_bin = [z_sorted.min()]
+            one_bin_count = z_sorted.shape[0] / bins
+            for i in range(1, bins):
+                idx = i * one_bin_count
+                if np.all(np.diff(z_sorted[idx-1:idx+2]) != 0):
+                    z_bin.append(z_sorted[idx])
+                elif np.any(np.diff(z_sorted[idx-1:idx+2]) == 0):
+                    where = np.where(np.diff(z_sorted[idx-1:idx+2]) != 0)[0]
+                    expand_idx = 1
+                    while where.size == 0:
+                        where = np.where(np.diff(z_sorted[idx-expand_idx:idx+1+expand_idx]) != 0)[0]
+                        expand_idx += 1
+                    if where[0] == 0:
+                        z_bin.append(z_sorted[idx-expand_idx])
+                    else:
+                        z_bin.append(z_sorted[idx+expand_idx])
+            z_bin.append(z_sorted.max())
+            z_bin = np.array(z_bin)
+            z_bins.append(np.array(z_bin))
+
+        # create EQQ bins -- variables
+        x_sorted = np.sort(x)
+        x_bin = [x.min()]
+        one_bin_count = x.shape[0] / bins
+        for i in range(1, bins):
+            idx = i * one_bin_count
+            if np.all(np.diff(x_sorted[idx-1:idx+2]) != 0):
+                x_bin.append(x_sorted[idx])
+            elif np.any(np.diff(x_sorted[idx-1:idx+2]) == 0):
+                where = np.where(np.diff(x_sorted[idx-1:idx+2]) != 0)[0]
+                expand_idx = 1
+                while where.size == 0:
+                    where = np.where(np.diff(x_sorted[idx-expand_idx:idx+1+expand_idx]) != 0)[0]
+                    expand_idx += 1
+                if where[0] == 0:
+                    x_bin.append(x_sorted[idx-expand_idx])
+                else:
+                    x_bin.append(x_sorted[idx+expand_idx])
+        x_bin.append(x.max())
+        x_bin = np.array(x_bin)
+
+        y_sorted = np.sort(y)
+        y_bin = [y.min()]
+        one_bin_count = y.shape[0] / bins
+        for i in range(1, bins):
+            idx = i * one_bin_count
+            if np.all(np.diff(y_sorted[idx-1:idx+2]) != 0):
+                y_bin.append(y_sorted[idx])
+            elif np.any(np.diff(y_sorted[idx-1:idx+2]) == 0):
+                where = np.where(np.diff(y_sorted[idx-1:idx+2]) != 0)[0]
+                expand_idx = 1
+                while where.size == 0:
+                    where = np.where(np.diff(y_sorted[idx-expand_idx:idx+1+expand_idx]) != 0)[0]
+                    expand_idx += 1
+                if where[0] == 0:
+                    y_bin.append(y_sorted[idx-expand_idx])
+                else:
+                    y_bin.append(y_sorted[idx+expand_idx])
+        y_bin.append(y.max())
+        y_bin = np.array(y_bin)
+
+        # create multidim bins for histogram
+        xyz_bins = [x_bin, y_bin]
+        [xyz_bins.append(cond_bins) for cond_bins in z_bins]
+
+        xz_bins = [x_bin]
+        [xz_bins.append(cond_bins) for cond_bins in z_bins]
+
+        yz_bins = [y_bin]
+        [yz_bins.append(cond_bins) for cond_bins in z_bins]
+
     
     elif algorithm == 'EQD':
         # bins are just integer
