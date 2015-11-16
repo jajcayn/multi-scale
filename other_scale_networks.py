c9cfa9b6495e2b4d57003dd26f0d7eec54b12644
Nikola Jajcay <jajcay@cs.cas.cz>
2015-11-17 00:24:53 +0100

    conditioned on NINO34

---
+++ other_scale_networks.py
@@ -0,0 +1,51 @@
+from scale_network import ScaleSpecificNetwork
+from datetime import date
+from multiprocessing import Pool
+
+
+WORKERS = 10
+PERIOD = 4
+
+def load_nino34_wavelet_phase(fname, start_date, end_date, anom = True):
+    from src.data_class import DataField
+    import src.wavelet_analysis as wvlt
+    from datetime import date
+    raw = np.loadtxt('/home/nikola/Work/phd/data/nino34monthly.txt')
+    data = []
+    time = []
+    for y in range(raw.shape[0]):
+        for m in range(1,13):
+            dat = float(raw[y, m])
+            data.append(dat)
+            time.append(date(int(raw[y]), m, 1).toordinal())
+
+    g = DataField(data = np.array(data), time = np.array(time))
+    g.location = "NINO3.4"
+    g.select_date(start_date, end_date)
+
+    if anom:
+        g.anomalise()
+
+    k0 = 6. # wavenumber of Morlet wavelet used in analysis
+    fourier_factor = (4 * np.pi) / (k0 + np.sqrt(2 + np.power(k0,2)))
+    per = PERIOD * 12 # frequency of interest
+    s0 = per / fourier_factor # get scale
+
+    wave, _, _, _ = wvlt.continous_wavelet(data, 1, False, wvlt.morlet, dj = 0, s0 = s0, j1 = 0, k0 = 6.)
+    phase = np.arctan2(np.imag(wave), np.real(wave))[0, :]
+
+    return phase
+
+
+
+
+nino34_phase = load_nino34_wavelet_phase()
+net = ScaleSpecificNetwork('/home/nikola/Work/phd/data/air.mon.mean.levels.nc', 'air', 
+                       date(1948,1,1), date(2014,1,1), None, None, 0, 'monthly', anom = False)
+pool = Pool(WORKERS)             
+net.wavelet(PERIOD, get_amplitude = False, pool = pool)
+print "wavelet on data done"
+pool.close()
+net.get_adjacency_matrix_conditioned(nino34_phase, use_queue = True, num_workers = WORKERS)
+print "estimating adjacency matrix done"
+net.save_net('networks/NCEP-SAT4y-phase-adjmatCMIEQQcondNINOphase.bin', only_matrix = True)
