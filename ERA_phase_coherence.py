b8d7f2412e697e0613e21a32048525233402cfc1
Nikola Jajcay <jajcay@cs.cas.cz>
2015-12-02 13:32:16 +0100

    fix

--- ERA_phase_coherence.py
+++ ERA_phase_coherence.py
@@ -8,30 +8,30 @@ import numpy as np
 WORKERS = 10
 
 # print "computing SATA phase ERA data networks..."
-# to_do = [['MIGAU', 8], ['MIGAU', 4], ['MIGAU', 6], ['MIGAU', 11], ['MIGAU', 15],
-#             ['MIEQQ', 4], ['MIEQQ', 6], ['MIEQQ', 8], ['MIEQQ', 11], ['MIEQQ', 15]]
+to_do = [['MIGAU', 8], ['MIGAU', 4], ['MIGAU', 6], ['MIGAU', 11], ['MIGAU', 15]]
+            # ['MIEQQ', 4], ['MIEQQ', 6], ['MIEQQ', 8], ['MIEQQ', 11], ['MIEQQ', 15]]
 
-# for do in to_do:
-    # METHOD = do[0]
-    # PERIOD = do[1]
+for do in to_do:
+    METHOD = do[0]
+    PERIOD = do[1]
     # print("computing for %d period using %s method" % (PERIOD, METHOD))
-net = ScaleSpecificNetwork('/home/nikola/Work/phd/data/ERAconcat.t2m.mon.means.1958-2014.bin', None, 
-                   date(1958,1,1), date(2014,1,1), None, None, None, 'monthly', anom = True, pickled = True)
-pool = Pool(WORKERS)             
-net.wavelet(PERIOD, get_amplitude = False, pool = pool)
-net.get_continuous_phase(pool = pool)
-print "wavelet on data done"
-pool.close()
+    net = ScaleSpecificNetwork('/home/nikola/Work/phd/data/ERAconcat.t2m.mon.means.1958-2014.bin', None, 
+                       date(1958,1,1), date(2014,1,1), None, None, None, 'monthly', anom = True, pickled = True)
+    pool = Pool(WORKERS)             
+    net.wavelet(PERIOD, get_amplitude = False, pool = pool)
+    net.get_continuous_phase(pool = pool)
+    print "wavelet on data done"
+    pool.close()
 
-net2 = ScaleSpecificNetwork('/home/nikola/Work/phd/data/air.mon.mean.levels.nc', 'air', 
-                           date(1958,1,1), date(2014,1,1), None, None, 0, 'monthly', anom = True)
-pool = Pool(WORKERS)             
-net2.wavelet(PERIOD, get_amplitude = False, pool = pool)
-net2.get_continuous_phase(pool = pool)
-pool.close()
+    net2 = ScaleSpecificNetwork('/home/nikola/Work/phd/data/air.mon.mean.levels.nc', 'air', 
+                               date(1958,1,1), date(2014,1,1), None, None, 0, 'monthly', anom = True)
+    pool = Pool(WORKERS)             
+    net2.wavelet(PERIOD, get_amplitude = False, pool = pool)
+    net2.get_continuous_phase(pool = pool)
+    pool.close()
 
-phase_diffs = net.phase - net2.phase
-print phase_diffs.shape
+    phase_diffs = net.phase - net2.phase
+    print phase_diffs.shape
 
 # import cPickle
 # with open("networks/NCEP-ERA-phase_diff.bin", "wb") as f:
