e42b4ed8b551f4aeb29567f304ce5f80f251b1fe
Nikola Jajcay <jajcay@cs.cas.cz>
2016-12-21 14:51:23 +0100

    fix

--- scale_net_surrs.py
+++ scale_net_surrs.py
@@ -1,9 +1,9 @@
-import sys
-sys.path.append("/Users/nikola/work-ui/multi-scale/")
+# import sys
+# sys.path.append("/Users/nikola/work-ui/multi-scale/")
 from scale_network import ScaleSpecificNetwork
 from datetime import date
 from pathos.multiprocessing import Pool
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import src.wavelet_analysis as wvlt
 import numpy as np
 from src.surrogates import SurrogateField
@@ -11,7 +11,8 @@ from src.surrogates import SurrogateField
 
 NUM_SURR = 100
 
-fname = "/Users/nikola/work-ui/data/NCEP/air.mon.mean.levels.nc"
+# fname = "/Users/nikola/work-ui/data/NCEP/air.mon.mean.levels.nc"
+fname = "/home/nikola/Work/phd/data/air.mon.mean.levels.nc"
 net = ScaleSpecificNetwork(fname, 'air', date(1948,1,1), date(2016,1,1), [-60, 0], [40, 100], level = 0, dataset = "NCEP", 
                 sampling = 'monthly', anom = False)
 
@@ -20,7 +21,7 @@ a = net.get_seasonality(detrend = True)
 surrs.copy_field(net)
 net.return_seasonality(a[0], a[1], a[2])
 
-pool = Pool(5)
+pool = Pool(20)
 net.wavelet(8, 'y', cut = 1, pool = pool)
 net.get_adjacency_matrix(net.amplitude, method = "MIEQQ", num_workers = 0, pool = pool, use_queue = False)
 pool.close()
@@ -32,7 +33,7 @@ surrs_adj_matrices = []
 
 for i in range(NUM_SURR):
     print("surr %d/%d computing..." % (i+1, NUM_SURR))
-    pool = Pool(5)
+    pool = Pool(20)
     surrs.construct_fourier_surrogates_spatial(pool = pool)
     surrs.add_seasonality(a[0], a[1], a[2])
 
