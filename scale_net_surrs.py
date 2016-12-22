33f24c2c7fdd7eb2e00a5f67f52bff5e20556ac9
Nikola Jajcay <jajcay@cs.cas.cz>
2016-12-22 15:55:59 +0100

    phases

--- scale_net_surrs.py
+++ scale_net_surrs.py
@@ -9,7 +9,7 @@ import numpy as np
 from src.surrogates import SurrogateField
 
 
-NUM_SURR = 100
+NUM_SURR = 1000
 
 # fname = "/Users/nikola/work-ui/data/NCEP/air.mon.mean.levels.nc"
 fname = "/home/nikola/Work/phd/data/air.mon.mean.levels.nc"
@@ -23,7 +23,7 @@ net.return_seasonality(a[0], a[1], a[2])
 
 pool = Pool(20)
 net.wavelet(8, 'y', cut = 1, pool = pool)
-net.get_adjacency_matrix(net.amplitude, method = "MIEQQ", num_workers = 0, pool = pool, use_queue = False)
+net.get_adjacency_matrix(net.phase, method = "MIEQQ", num_workers = 0, pool = pool, use_queue = False)
 pool.close()
 pool.join()
 
@@ -34,17 +34,17 @@ surrs_adj_matrices = []
 for i in range(NUM_SURR):
     print("surr %d/%d computing..." % (i+1, NUM_SURR))
     pool = Pool(20)
-    surrs.construct_fourier_surrogates_spatial(pool = pool)
+    surrs.construct_fourier_surrogates(pool = pool)
     surrs.add_seasonality(a[0], a[1], a[2])
 
     net.data = surrs.get_surr()
     net.wavelet(8, 'y', cut = 1, pool = pool)
-    net.get_adjacency_matrix(net.amplitude, method = "MIEQQ", num_workers = 0, pool = pool, use_queue = False)
+    net.get_adjacency_matrix(net.phase, method = "MIEQQ", num_workers = 0, pool = pool, use_queue = False)
     pool.close()
     pool.join()
     surrs_adj_matrices.append(net.adjacency_matrix)
 
 import cPickle
-with open("8yr-scale-net-surrs-test-spatial.bin", "wb") as f:
+with open("8yr-phase-scale-net-surrs-test.bin", "wb") as f:
     cPickle.dump({'data' : data_adj_matrix, 'surrs' : surrs_adj_matrices}, f, protocol = cPickle.HIGHEST_PROTOCOL)
 
